80806b7471807a77458cf3356aca370eb4402336
nicholas <nicholas.us.03@gmail.com>
2024-03-31 13:22:25 +0300

    title && meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,20 @@
 export default defineNuxtConfig({
     devtools: {enabled: true},
     modules: ['@nuxtjs/tailwindcss'],
+    app: {
+        head: {
+            title: 'Nuxt Dojo',
+            meta: [
+                {name: 'description', content: 'Everything about Nuxt 3'}
+            ],
+            link: [
+                {
+                    rel: "stylesheet",
+                    href: 'https://material-icons.github.io/material-icons-font/css/all.css'
+                }
+            ]
+        }
+    },
     css: ['~/assets/css/tailwind.css'],
     tailwindcss: {
         cssPath: '~/assets/css/tailwind.css',
